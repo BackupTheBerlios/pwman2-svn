bbd99195766469d0dc43c624638402246a638b50
ivan <ivan@cdcd9f2a-a2f7-0310-94d2-b51a390b40fe>
2005-05-27 13:59:58 +0000

    meh

---
+++ trunk/pwlib/db/drivers/BerkeleyDatabase.py
@@ -0,0 +1,32 @@
+from pwdb.PwmanDatabase import PwmanDatabase,PwmanDatabaseException
+
+class BerkeleyDatabase(PwmanDatabase):
+    """Berkeley Database implementation"""
+    
+    def __init__(self, params):
+        
+        pass
+
+    def _open(self):
+        pass
+
+    def _put(self, name, data):
+        pass
+
+    def _get(self, name):
+        pass
+
+    def _delete(self, name):
+        pass
+
+    def _close(self):
+        pass
+
+    def _mklist(self, name):
+        pass
+
+    def _rmlist(self, name):
+        pass
+
+    def _list(self):
+        pass
